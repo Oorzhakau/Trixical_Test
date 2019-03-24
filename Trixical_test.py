c3496f1a232e686cc3abf141a4c6b5b8de32990e
Oorzhakau <oorzhakau@yandex.ru>
2019-03-25 00:20:30 +0300

    update class

--- Trixical_test.py
+++ Trixical_test.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
 from scipy.interpolate import interp1d
 import numpy as np
-import xlwt
+import shutil
+import openpyxl
 
 #book = xlwt.Workbook(encoding="utf-8")
 #sheet1 = book.add_sheet("Sheet 1")
@@ -9,15 +10,16 @@ import xlwt
 n = 100 
 
 class LineBuilder:
-    def __init__(self, line):
+    def __init__(self, line, bh = 'x', depth = '1', sigma3 = 0.1):
         self.line = line
+        self.bh = bh
+        self.depth = depth
+        self.sigma3 = sigma3
         self.xs = list(line.get_xdata())
         self.ys = list(line.get_ydata())
         self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
         self.cid1 = line.figure.canvas.mpl_connect('key_press_event', self.press)
-        self.book = xlwt.Workbook(encoding="utf-8")
-        self.sheet = self.book.add_sheet("Sheet 1") 
-
+        
     def __call__(self, event):
         print('click', event)
         if event.inaxes!=self.line.axes: return
@@ -28,8 +30,12 @@ class LineBuilder:
         
     def press(self, event):
         if event.key == 'x':
+            x1 = self.xs[0]
+            y1 = self.ys[0]
             self.xs = list()
             self.ys = list()
+            self.xs.append(x1)
+            self.ys.append(y1)
             self.line.set_data(self.xs, self.ys)
             self.line.figure.canvas.draw()
         if event.key == 'w':
@@ -38,11 +44,19 @@ class LineBuilder:
             ynew = f(xnew)
             self.line.set_data(xnew, ynew)
             self.line.figure.canvas.draw()
+            shutil.copy('Exp.xlsx',self.bh+"_"+self.depth+".xlsx")
+            self.wb = openpyxl.load_workbook(self.bh+"_"+self.depth+".xlsx")
+            
+            self.ws = self.wb['Sheet 1']
+            self.ws['C20'] = self.bh 
+            self.ws['C21'] = self.depth
+            self.ws['C27'] = self.sigma3
             for num in range(len(xnew)):
-                row = self.sheet.row(num)
-                row.write(0,xnew[num])
-                row.write(1,ynew[num])
-            self.book.save("test.xls")
+                self.ws['R'+str(10+num)] = xnew[num]
+                self.ws['S'+str(10+num)] = ynew[num]
+                #self.ws.cell(row = (9 + num), column = 18, value = xnew[num])
+                #self.ws.cell(row = (9 + num), column = 19, value = ynew[num])
+            self.wb.save(self.bh+"_"+self.depth+".xlsx")
 
 if __name__ == "__main__":
     fig = plt.figure()
